cd0c52c212f6aec49a7adaaa11d52d6ab28cd32c
oguzhan <ocetin169@gmail.com>
2022-12-25 22:36:57 +0300

    add callback risk item factor

--- lib/screens/chat_simulation/forms/fall_risk_scale_form.dart
+++ lib/screens/chat_simulation/forms/fall_risk_scale_form.dart
@@ -24,31 +24,41 @@ class RiskFactor extends StatefulWidget {
 
 class _RiskFactorState extends State<RiskFactor> {
   int totalScore = 0;
+
+
   @override
   Widget build(BuildContext context) {
+
+    void udapteTotalScore(int score) {
+      setState(() {
+        totalScore += score;
+      });
+
+    }
+
     return SingleChildScrollView(
         child: Column(
           children: [
             RiskFactorHeader(),
-            RiskFactorItem(title:"65 yaş üstü",score:1),
-            RiskFactorItem(title:"Bilinci kapalı",score:1),
-            RiskFactorItem(title:"Son bir ay içinde düşme öyküsü var",score:1),
-            RiskFactorItem(title:"Kronik hastalık öyküsü var",score:1),
-            RiskFactorItem(title:"Ayakta/Yürürken fiziksel desteğe ihtiyacı var",score:1),
-            RiskFactorItem(title:"Ürimer/Tekal kontinans bozukluğu var",score:1),
-            RiskFactorItem(title:"Görme durumu bozukluğu var",score:1),
-            RiskFactorItem(title:"4'den fazla ilaç kullanımı var ",score:1),
-            RiskFactorItem(title:"Hastaya bağlı 3'ün altında bakım ekipmanı var",score:1),
-            RiskFactorItem(title:"Yatak korkulukları bulunmuyor/çalışmıyor",score:1),
-            RiskFactorItem(title:"Yürüme alanlarında fiziksel engel(ler) var",score:1),
-            RiskFactorItem(title:"Bilinç açık koopere değil",score:5),
-            RiskFactorItem(title:"Ayakta yürürken denge problemi var",score:5),
-            RiskFactorItem(title:"Baş dönmesi var",score:5),
-            RiskFactorItem(title:"Ortostatik hipotansiyonu var",score:5),
-            RiskFactorItem(title:"Görme engeli var",score:5),
-            RiskFactorItem(title:"Bedensel engeli var",score:5),
-            RiskFactorItem(title:"Hastaya bağlı 3 ve üstü bakım ekipmanı var",score:5),
-            RiskFactorItem(title:"Son 1 hafta içinde riskli ilaç kullanımı var",score:5),
+            RiskFactorItem(title:"65 yaş üstü",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Bilinci kapalı",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Son bir ay içinde düşme öyküsü var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Kronik hastalık öyküsü var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Ayakta/Yürürken fiziksel desteğe ihtiyacı var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Ürimer/Tekal kontinans bozukluğu var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Görme durumu bozukluğu var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"4'den fazla ilaç kullanımı var ",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Hastaya bağlı 3'ün altında bakım ekipmanı var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Yatak korkulukları bulunmuyor/çalışmıyor",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Yürüme alanlarında fiziksel engel(ler) var",score:1,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Bilinç açık koopere değil",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Ayakta yürürken denge problemi var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Baş dönmesi var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Ortostatik hipotansiyonu var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Görme engeli var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Bedensel engeli var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Hastaya bağlı 3 ve üstü bakım ekipmanı var",score:5,updateTotalScore:udapteTotalScore),
+            RiskFactorItem(title:"Son 1 hafta içinde riskli ilaç kullanımı var",score:5,updateTotalScore:udapteTotalScore),
             SizedBox(
               width: 250,
               child: TextField(
@@ -138,8 +148,9 @@ class _DeterminingRiskLevelState extends State<DeterminingRiskLevel> {
 
 
 class RiskFactorItem extends StatefulWidget {
-  const RiskFactorItem({Key? key,required this.title,required this.score}) : super(key: key);
+  const RiskFactorItem({Key? key,required this.title,required this.score,required this.updateTotalScore}) : super(key: key);
 
+  final Function updateTotalScore;
   final String title;
   final int score;
   @override
@@ -167,7 +178,10 @@ class _RiskFactorItemState extends State<RiskFactorItem> {
                 value: isChecked,
                 onChanged: (bool? value) {
                   setState(() {
+
+                    widget.updateTotalScore(value == true ? widget.score : -widget.score);
                     isChecked = value!;
+
                   });
                 })
           ],
